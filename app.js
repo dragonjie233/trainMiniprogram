4ff756acc26532c9d9dd9789f0f4b9078e6ffb08
dragonjie233 <longjie.qwq@qq.com>
2024-01-31 12:21:11 +0800

    1.0.3 增加更新检测

--- app.js
+++ app.js
@@ -19,6 +19,36 @@ App({
     dictLength: 0,
     selected: 0
   },
+  onLaunch() {
+    this.autoUpdate()
+  },
+  /** 自动更新 */
+  autoUpdate () {
+    const um = wx.getUpdateManager()
+    um.onCheckForUpdate(res => {
+      if (!res.hasUpdate) return;
+
+      um.onUpdateReady(() => {
+        wx.showModal({
+          title: '更新提示',
+          content: '新版本已经准备好，是否重启应用？',
+          success: function (res) {
+            if (res.confirm) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              um.applyUpdate()
+            }
+          }
+        })
+      })
+
+      um.onUpdateFailed(() => {
+        wx.showModal({
+          title: '新版本已上线',
+          content: '请您删除当前小程序，重新打开'
+        })
+      })
+    })
+  },
   /**
    * Toast消息提示
    * @param {*} code 请求结果码
